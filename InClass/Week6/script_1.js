fcea0f8111e3a797ee35096e7e5a70b092560e44
karaholinski <kholinski3@gmail.com>
2018-04-04 22:04:59 -0400

    proj
    proj

--- InClass/Week6/script_1.js
+++ InClass/Week6/script_1.js
@@ -11,7 +11,7 @@ var plot1 = d3.select('#plot1') // if we select a html id #name, if we select a 
     .attr('height', height + margin.t + margin.b);
 
 // function to draw the map
-
+var path = d3.geoPath()
 // queue data files, parse them and use them
 var queue = d3.queue()
     .defer(d3.csv, "../data/data.csv", parseData)
@@ -19,17 +19,61 @@ var queue = d3.queue()
     .await(dataloaded);
 
 function dataloaded (err,data,map){
-
+    console.log(data)
+    console.log(map)
     // get max and min values of data
-
+    var extentData = d3.extent(data,function(d){return d.total});
+    var extentPerCapitaData = d3.extent(data,function(d){
+        var stateID = d.id;
+        var statePopulation = populationPerState.get(stateID).estimate2017
+        return d.total.statePopulation;
+    })
+    var colorScale = d3.scaleLinear().domain(extentData).range(['#ffc5c0','#ab0405']);
     // scale Color for the map
 
     // Bind the data to the SVG and create one path per GeoJSON feature
+    plot1.selectAll(".state")
+        .data(topojson.feature(map,map.objects.states).features)
+        .enter()
+        .append('path')
+        .attr("d",path)
+        .attr('class',"state")
+        .style("fill",function(d){
+            var mapID = +d.id;
+            var color = "#000"
+            var totalPopulation = populationPerState.get(mapID).estimate2017
+            data.forEach(function(e){
+                if (e.id.toString() === mapID.toString()){
+                    color = colorScale(e.total)
 
+                }
+            });
+            return color
+        });
 }
 
 
 
 function parseData(d){
+    var id = d.Id.split("US")[1];
+    return { 
+        id: +id,
+        state: d.state,
+        total: +d["Total; Estimate; Population 3 years and over enrolled in school"]
+        
+    }
+}
 
+function parsePopulation(d){
+    var id;
+    if (d.id.split !== ''){
+        id = d.id.split("US")[1]
+    }else{
+        id = d["Geographic Area"]
+    }
+    populationPerState.set(id, {
+        state: d['Geographic Area'],
+        april2010: +d["April 1, 2010, census"],
+        estimate2017: +d["Estimate 2017"]
+    })
 }
